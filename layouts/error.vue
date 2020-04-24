4e05a4b5b44efa26fff3273217ded6790dbee0dc
edison13571 <740336406@qq.com>
2020-04-24 14:05:39 +0800

    404 page

---
+++ layouts/error.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="error temp">
+    <div class="title" v-if="error.statusCode == 404">404 - Not Found</div>
+    <div class="title" v-else>500 - 服务器错误</div>
+    <div class="back">
+      <ul>
+        <li @click="$router.back(-1)">返回上一页</li>
+        <nuxt-link to="/">
+          <li>查看首页</li>
+        </nuxt-link>
+      </ul>
+    </div>
+  </div>
+</template>
+<script>
+  export default {
+    props: ['error']
+  }
+</script>
+
+<style lang="scss">
+  .error{
+    li{
+      list-style: none;
+    }
+    .back ul{
+      @include flexJustify(space-between);
+      width: 300px;
+      cursor: pointer;
+    }
+    .title{
+      margin-bottom: 50px;
+    }
+  }
+
+</style>
